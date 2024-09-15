c7698de078c286c689d8daf6955d81ca5985bc60
Zvonimir Alpeza <zvonimir.alpeza@fpmoz.sum.ba>
2024-09-15 17:11:44 +0200

    Popraljven CORS problem

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,10 @@ const cors = require('cors');
 const app = express();
 const port = 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT']
+}));
 
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
